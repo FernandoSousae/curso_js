805b0313e0462703c7f8033be67dfd9990f4b6ca
FernandoSousagvx <fer.pe.sou.86@gmail.com>
2024-09-01 13:54:12 -0300

    Initial commit

---
+++ aula12ex/ex015 copy/script.js
@@ -0,0 +1,41 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById("txtano")
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert("[ERRO] Verifique os dados e tente novamente!")
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'crianca_m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem_m.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'adulto_m.png')
+            } else {
+                img.setAttribute('src', 'idoso_m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'crianca_f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'jovem_f.png')
+            } else if (idade < 60) {
+                img.setAttribute('src', 'adulto_f.png')
+            } else {
+                img.setAttribute('src', 'idoso_m.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
